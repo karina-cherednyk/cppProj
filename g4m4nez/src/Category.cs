adf31f93d067b18d6464ca360ae252ca0d01a320
Roman Burakov <buracov.r@gmail.com>
2021-03-07 20:01:29 +0200

    add category

--- g4m4nez/src/Category.cs
+++ g4m4nez/src/Category.cs
@@ -1,4 +1,23 @@
 ﻿public class Category
 {
+    private string icon;
+    public string Icon
+    {
+        get { return icon; }
+        set { icon = value; }
+    }
 
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    private string description;
+    public string Description
+    {
+        get { return description; }
+        set { description = value; }
+    }
 }
