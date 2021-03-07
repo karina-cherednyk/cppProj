b8b68d93e057c0d4d4a00a6d64969f6627bac914
Volodymyr Barannik <volodymyr.barannik@ukma.edu.ua>
2021-03-07 20:33:59 +0200

    updated email test

--- BusinessLayer.Tests/EmailTest.cs
+++ BusinessLayer.Tests/EmailTest.cs
@@ -10,15 +10,21 @@ namespace BusinessLayer.Tests
         {
             //Arrange
             var expected1 = "boublik";
-            var expected2 = "";
+            var expected2 = "boublik";
+            var expected3 = "";
+            var expected4 = "";
 
             //Act
-            var actual1 = new Email("boublik");
-            var actual2 = new Email("");
+            var actual1 = new Email("boublik", "").MailName();
+            var actual2 = new Email("boublik", "asdavsdvasdva").MailName();
+            var actual3 = new Email("", "").MailName();
+            var actual4 = new Email("", "boublik").MailName();
 
             //Assert
             Assert.Equal(expected1, actual1);
             Assert.Equal(expected2, actual2);
+            Assert.Equal(expected3, actual3);
+            Assert.Equal(expected4, actual4);
         }
     }
 }
