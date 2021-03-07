2d75eeff832409cb9d3f1c57af21468f8ae2773e
Roman Burakov <buracov.r@gmail.com>
2021-03-07 20:24:24 +0200

    add email

--- g4m4nez/src/Email.cs
+++ g4m4nez/src/Email.cs
@@ -3,6 +3,28 @@
     public class Email
     {
         private string mailname;
+        public string MainName
+        {
+            get { return mailname; }
+            set { mailname = value; }
+        }
+
         private string domain;
+        public string Domain
+        {
+            get { return domain; }
+            set { domain = value; }
+        }
+
+        public override string ToString()
+        {
+            return mailname + "@" + domain;
+        }
+
+        public Email(string mailname, string domain)
+        {
+            MainName = mailname;
+            Domain = domain;
+        }
     }
 }
