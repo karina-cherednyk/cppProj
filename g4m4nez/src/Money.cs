3f03143904e63e44b58ed2bf6c1e2596948a26ee
Volodymyr Barannik <volodymyr.barannik@ukma.edu.ua>
2021-03-07 22:21:28 +0200

    added money tests

--- g4m4nez/src/Money.cs
+++ g4m4nez/src/Money.cs
@@ -40,7 +40,7 @@ namespace BusinessLayer
             }
         }
 
-        public static Money operator -(Money a, Money b)
+        public static Money operator-(Money a, Money b)
         {
             if (a.Currency != b.Currency)
             {
