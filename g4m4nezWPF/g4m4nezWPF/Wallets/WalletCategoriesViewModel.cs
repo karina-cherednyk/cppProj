03a20baa73f5c40eeb13e3c4cde3e267d226a5a4
Roman Burakov <buracov.r@gmail.com>
2021-04-17 18:13:13 +0300

    add category & transaction view models

---
+++ g4m4nezWPF/g4m4nezWPF/Wallets/WalletCategoriesViewModel.cs
@@ -0,0 +1,36 @@
+﻿using g4m4nez.BusinessLayer;
+using g4m4nez.Models;
+using g4m4nez.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace g4m4nez.GUI.WPF.Wallets
+{
+    class WalletCategoriesViewModel
+    {
+        private readonly WalletService _service;
+
+        private readonly Wallet _wallet;
+        public Wallet FromWallet => _wallet;
+
+        private Category _selectedCategory;
+        public static ObservableCollection<Category> WalletCategoriesAdded { get; set; }
+        public static ObservableCollection<Category> WalletCategoriesAvailable { get; set; }
+
+        public async void AddWalletCategory()
+        {
+            await Task.Run(() => _service.AddCategory(CurrentSession.User.Guid, _wallet.Guid, _selectedCategory));
+        }
+
+        public async void RemoveWalletCategory()
+        {
+            await Task.Run(() => _service.RemoveCategory(CurrentSession.User.Guid, _wallet.Guid, _selectedCategory));
+        }
+
+    }
+
+}
